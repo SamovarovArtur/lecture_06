b72c95ec8554048766bd97976653b46a8221429f
art <samovarovartur@gmail.com>
2018-05-03 22:18:41 +0300

    home work lesson_06

--- src/arrayMethods.js
+++ src/arrayMethods.js
@@ -1,5 +1,45 @@
-window.every = function () {};
-window.some = function () {};
-window.forEach = function () {};
-window.filter = function () {};
-window.map = function () {};
+window.every = function (array, callback) {
+	var result;
+	for (var i = 0;i < array.length; i++) {
+		if (callback(array[i],i,array)){
+			result = true;
+		} else {
+			result = false;
+			break;
+		}
+	}
+	return result;
+};
+window.some = function (array, callback) {
+	var result;
+	for (var i = 0;i < array.length; i++) {
+		if (callback(array[i],i,array)){
+			result = true;
+			break;
+		} else {
+			result = false;
+		}
+	}
+	return result;
+};
+window.forEach = function (array, callback) {
+	var result = '';
+	for (var i = 0;i < array.length; i++) {
+		callback((array[i]),i,array)
+	}
+};
+window.filter = function (array, callback) {
+	var result = [];
+	for(var i = 0;i <array.length; i++){
+		if(callback(array[i],i,array)){
+			result.push(array[i]);
+		}else{continue;}
+	}
+	return result
+};
+window.map = function (array, callback) {
+	var result = [];
+    for(var i = 0; i < array.length;i++){
+    	result.push(callback(array[i],i,array))
+    }return result
+};
